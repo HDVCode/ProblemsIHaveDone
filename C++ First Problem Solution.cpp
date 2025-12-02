3a7d6fcbe997b448f0d5eef8985155a7f9f02388
HDVCode <hector.villanueva.davila@gmail.com>
2025-12-02 22:31:36 +0000

    Refactor main function for clarity and efficiency

--- C++ First Problem Solution.cpp
+++ C++ First Problem Solution.cpp
@@ -26,58 +26,55 @@ void isItPosibleToReachFrom(vector<vector<int>>& nodes, int start, vector<bool>&
 
 int main() {
 
-    while (true) { 
+    int amountOfRestaurantes = 0;
+    int amountOfLinesToProccess = 0;
 
-        int amountOfRestaurantes = 0;
-        int amountOfLinesToProccess = 0;
+    cin >> amountOfRestaurantes >> amountOfLinesToProccess;
+    if (!cin) return 0; 
 
-        cin >> amountOfRestaurantes >> amountOfLinesToProccess;
-        if (!cin) return 0; 
+    vector<vector<int>> nodes(amountOfRestaurantes);
+    vector<vector<int>> nodesReversed(amountOfRestaurantes);
 
-        vector<vector<int>> nodes(amountOfRestaurantes);
-        vector<vector<int>> nodesReversed(amountOfRestaurantes);
-
-        for (int i = 0; i < amountOfLinesToProccess; i++) {
-            int a, b;
-            cin >> a >> b;
-            nodes[a-1].push_back(b-1);
-            nodesReversed[b-1].push_back(a-1);
-        }
-
-        int cases = 0;
-        cin >> cases;
-
-        for (int i = 0; i < cases; i++) {
-
-            int startingNode, endingNode;
-            cin >> startingNode >> endingNode;
-
-            startingNode--;
-            endingNode--;
-
-            vector<bool> reachableFromStart(amountOfRestaurantes, false);
-            vector<bool> reachableFromEnd(amountOfRestaurantes, false);
-
-            isItPosibleToReachFrom(nodes, startingNode, reachableFromStart);
-            isItPosibleToReachFrom(nodesReversed, endingNode, reachableFromEnd);
-
-            if (!reachableFromStart[endingNode]) {
-                cout << "IMPOSIBLE\n";
-                continue;
-            }
-
-            int result = 0;
-
-            for (int j = 0; j < amountOfRestaurantes; j++) {
-                if (reachableFromStart[j] && reachableFromEnd[j])
-                    result++;
-            }
-
-            cout << result << "\n";
-        }
-
-        cout << "---\n";
+    for (int i = 0; i < amountOfLinesToProccess; i++) {
+        int a, b;
+        cin >> a >> b;
+        nodes[a-1].push_back(b-1);
+        nodesReversed[b-1].push_back(a-1);
     }
+
+    int cases = 0;
+    cin >> cases;
+
+    for (int i = 0; i < cases; i++) {
+
+        int startingNode, endingNode;
+        cin >> startingNode >> endingNode;
+
+        startingNode--;
+        endingNode--;
+
+        vector<bool> reachableFromStart(amountOfRestaurantes, false);
+        vector<bool> reachableFromEnd(amountOfRestaurantes, false);
+
+        isItPosibleToReachFrom(nodes, startingNode, reachableFromStart);
+        isItPosibleToReachFrom(nodesReversed, endingNode, reachableFromEnd);
+
+        if (!reachableFromStart[endingNode]) {
+            cout << "IMPOSIBLE\n";
+            continue;
+        }
+
+        int result = 0;
+
+        for (int j = 0; j < amountOfRestaurantes; j++) {
+            if (reachableFromStart[j] && reachableFromEnd[j])
+                    result++;
+        }
+
+        cout << result << "\n";
+    }
+
+    cout << "---\n";
 
     return 0;
 }
